b99f27eea9fc42b53adad99ce98e716d7194d35f
Chutikarn-Jon <144781526+Chutikarn-Jon@users.noreply.github.com>
2023-09-12 18:41:01 +0700

    Update water3.py

--- task-1/water3.py
+++ task-1/water3.py
@@ -9,31 +9,31 @@ def successors(s):
     x, y, z = s
 
     #Pour from the 8-liter bottle to the 5-litter bottle
-    if x>0 and y<5 ;
-        amount = min(x,5-y);
+    if x>0 and y<5:
+        amount = min(x,5-y)
         actions.append(((x-amount,y+amount,z),amount))
     #Pour from the 5-liter bottle to the 3-litter bottle
-    if y>0 and z<3;
-         amount = min(y,3-z);
+    if y>0 and z<3:
+        amount = min(y,3-z)
         actions.append(((x,y-amount,z+amount),amount))
     #Pour from the 3-liter bottle to the 8 -liter bottle
-    if z>0 and x<8;
-        amount = min(z,8-x);
+    if z>0 and x<8:
+        amount = min(z,8-x)
         actions.append(((x+amount,y,z-amount),amount))
     #Pour from the 5-liter bottle to the 3-liter bottle
-    if y>0 and z<3;
-        amount = min(y,3-z);
+    if y>0 and z<3:
+        amount = min(y,3-z)
         actions.append(((x,y-amount,z+amount),amount))
     #Pour from the 8-liter bottle to the 5-liter bottle
-    if x>0 and y<5;
-        amount = min(x,5-y);
+    if x>0 and y<5:
+        amount = min(x,5-y)
         actions.append(((x-amount,y+amount,z),amount))
     #Pour from the 5-liter bottle to the 3-liter bottle
-    if y>0 and z<3;
-        amount = min(y,3-z);
+    if y>0 and z<3:
+        amount = min(y,3-z)
         actions.append(((x,y-amount,z+amount),amount))
     #Pour from the 3-liter bottle to the 8-liter bottle
-    if z>0 and x<8;
-        amount = min(z,8-x);
+    if z>0 and x<8:
+        amount = min(z,8-x)
         actions.append(((x+amount,y,z-amount),amount))
     return actions
